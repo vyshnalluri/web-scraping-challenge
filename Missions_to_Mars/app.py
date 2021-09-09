4fba2b1777549132d6d2cd810b3b9aafc80f8198
Vysh Nalluri <vyshnalluri@gmail.com>
2021-09-09 12:13:50 -0400

    updated templates

---
+++ Missions_to_Mars/app.py
@@ -0,0 +1,28 @@
+from flask import Flask, render_template, redirect
+from flask_pymongo import PyMongo
+import scrape_mars
+
+app = Flask(__name__)
+
+# Use flask_pymongo to set up mongo connection
+app.config["MONGO_URI"] = "mongodb://localhost:27017/mars_app"
+mongo = PyMongo(app)
+
+
+@app.route("/")
+def index():
+    mars_data_dict = mongo.db.mars_data_dict.find_one()
+
+    # Return template and data
+    return render_template("index.html", mars_data_dict=mars_data_dict)
+
+@app.route("/scrape")
+def scrape():
+    mars_data_dict = scrape_mars.scrape()
+
+    mongo.db.mars_data_dict.update({}, mars_data_dict, upsert=True)
+
+    return redirect("/", code=302)
+
+if __name__ == "__main__":
+    app.run(debug=True)
